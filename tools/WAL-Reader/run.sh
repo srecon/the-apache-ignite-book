edc17db3fe157a5cba384c44f66793858ce63f00
Shamim <srecon@yandex.ru>
2018-07-28 22:47:01 +0300

    Update run.sh

--- tools/WAL-Reader/run.sh
+++ tools/WAL-Reader/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp "./ignite-dev-utils.jar:./ignite-indexing-2.6.0.jar:./ignite-core-2.6.0.jar:./h2-1.4.195.jar:./cache-api-1.0.0.jar" org.apache.ignite.development.utils.IgniteWalConverter 4096 /Users/shamim/Development/bigdata/ignite/2.6.0/work/db/wal/node00-fc36db73-ba1c-45a6-a545-0bbd363a8746
+java -cp "./ignite-dev-utils.jar:./ignite-indexing-2.6.0.jar:./ignite-core-2.6.0.jar:./h2-1.4.195.jar:./cache-api-1.0.0.jar" org.apache.ignite.development.utils.IgniteWalConverter 4096 $IGNITE_HOME/work/db/wal/node00-fc36db73-ba1c-45a6-a545-0bbd363a8746
