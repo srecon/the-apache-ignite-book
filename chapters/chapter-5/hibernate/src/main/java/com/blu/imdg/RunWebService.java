32450a90aa2d23c2a1e5421a44f011396d610c09
shamim <shamimapache@gmail.com>
2018-08-30 21:52:04 +0300

    Fix error on Hibernate l2 example

---
+++ chapters/chapter-5/hibernate/src/main/java/com/blu/imdg/RunWebService.java
@@ -0,0 +1,27 @@
+package com.blu.imdg;
+
+import com.blu.imdg.ws.WebService;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.xml.ws.Endpoint;
+
+/**
+ * Created by shamim on 24/06/16.
+ */
+public class RunWebService {
+    public static void main(String[] args) {
+        Ignite ignite = Ignition.start("ignite-client-config.xml");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-core.xml");
+
+        WebService service = (WebService) ctx.getBean("serviceBean");
+
+        Endpoint.publish("http://localhost:7001/invokeRules", service);
+        System.out.println("Server start in Port .. 7001");
+        System.out.println("Web service WSDL is available at http://localhost:7001/invokeRules?wsdl");
+    }
+}
+
