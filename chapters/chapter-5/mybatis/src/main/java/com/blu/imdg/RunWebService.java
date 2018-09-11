4f35ca18ab8e383339889b00a41177ca8c08efab
shamim <shamimapache@gmail.com>
2018-09-11 22:03:38 +0300

    add mybatis

---
+++ chapters/chapter-5/mybatis/src/main/java/com/blu/imdg/RunWebService.java
@@ -0,0 +1,22 @@
+package com.blu.imdg;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.xml.ws.Endpoint;
+
+
+public class RunWebService {
+    public static void main(String[] args) {
+        //Ignite ignite = Ignition.start("ignite-l2-config.xml");
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-core.xml");
+        WebServices services = (WebServices) ctx.getBean("servicesBean");
+
+        Endpoint.publish("http://localhost:7001/invokeRules", services);
+        System.out.println("Server start in Port .. 7001");
+        System.out.println("Web service WSDL is available at http://localhost:7001/invokeRules?wsdl");
+    }
+}
