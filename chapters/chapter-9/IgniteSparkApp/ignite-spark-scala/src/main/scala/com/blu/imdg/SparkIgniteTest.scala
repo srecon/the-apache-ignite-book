87e3c7b14577f1aedc3a96e5b38e1245c0b3d795
shamim <shamimapache@gmail.com>
2018-09-20 10:39:08 +0300

    add RDD

---
+++ chapters/chapter-9/IgniteSparkApp/ignite-spark-scala/src/main/scala/com/blu/imdg/SparkIgniteTest.scala
@@ -0,0 +1,25 @@
+package com.blu.imdg
+
+import org.apache.ignite.configuration._
+import org.apache.ignite.spark.{IgniteContext, IgniteRDD}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object RDDProducer extends App {
+  val conf = new SparkConf().setAppName("SparkIgniteProducer")
+  val sc = new SparkContext(conf)
+  //val ic = new IgniteContext[Int, Int](sc, () => new IgniteConfiguration())
+  val ic = new IgniteContext(sc, () => new IgniteConfiguration())
+  val sharedRDD: IgniteRDD[Int, Int] = ic.fromCache("IgniteRDD")
+  sharedRDD.savePairs(sc.parallelize(1 to 1000, 10).map(i => (i, i)))
+}
+
+object RDDConsumer extends App {
+  val conf = new SparkConf().setAppName("SparkIgniteConsume")
+  val sc = new SparkContext(conf)
+  //val ic = new IgniteContext[Int, Int](sc, () => new IgniteConfiguration())
+  val ic = new IgniteContext(sc, () => new IgniteConfiguration())
+  val sharedRDD = ic.fromCache[String, Int]("IgniteRDD")
+  val lessThanTwenty = sharedRDD.filter(_._2 < 20)
+
+  println("The count is:::::::::::: "+lessThanTwenty.count())
+}
---
+++ chapters/chapter-9/IgniteSparkApp/wordcount/src/main/java/com/blu/imdg/JavaWordCount.java
@@ -0,0 +1,60 @@
+package com.blu.imdg;
+
+import scala.Tuple2;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ * Run in Spark Shell : ./spark-submit --class com.blu.imdg.JavaWordCount --master spark://192.168.1.37:7077 ~/Development/workshop/github/ignite-book-code-samples/chapters/chapter-bigdata-spark/wordcount/target/wordcount-1.0-SNAPSHOT.jar ~/Downloads/CHANGES.txt ~/Downloads/
+ * */
+public final class JavaWordCount {
+    private static final Pattern SPACE = Pattern.compile(" ");
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length < 1) {
+            System.err.println("Usage: JavaWordCount <file>");
+            System.exit(1);
+        }
+
+        SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount");
+        JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+        JavaRDD<String> lines = ctx.textFile(args[0], 1);
+
+        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+            //@Override
+            public Iterable<String> call(String s) {
+                return Arrays.asList(SPACE.split(s));
+            }
+        });
+
+        JavaPairRDD<String, Integer> ones = words.mapToPair(new PairFunction<String, String, Integer>() {
+            //@Override
+            public Tuple2<String, Integer> call(String s) {
+                return new Tuple2<String, Integer>(s, 1);
+            }
+        });
+
+        JavaPairRDD<String, Integer> counts = ones.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            //@Override
+            public Integer call(Integer i1, Integer i2) {
+                return i1 + i2;
+            }
+        });
+
+        List<Tuple2<String, Integer>> output = counts.collect();
+        for (Tuple2<?,?> tuple : output) {
+            System.out.println(tuple._1() + ": " + tuple._2());
+        }
+        ctx.stop();
+    }
+}
