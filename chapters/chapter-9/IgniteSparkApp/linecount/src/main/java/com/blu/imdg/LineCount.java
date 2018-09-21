660ee0ea4da17758dcf02902bb0cdac1d38db972
shamim <shamimapache@gmail.com>
2018-09-21 16:07:35 +0300

    Fix line count

---
+++ chapters/chapter-9/IgniteSparkApp/linecount/src/main/java/com/blu/imdg/LineCount.java
@@ -0,0 +1,25 @@
+package com.blu.imdg;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
+
+public final class LineCount {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            System.err.println("Usage: JavaWordCount <file>");
+            System.exit(1);
+        }
+
+        String logFile = args[0];
+        SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+        Dataset<String> logData = spark.read().textFile(logFile).cache();
+
+        long numAs = logData.filter(s -> s.contains("a")).count();
+        long numBs = logData.filter(s -> s.contains("b")).count();
+
+        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+        spark.stop();
+    }
+}
