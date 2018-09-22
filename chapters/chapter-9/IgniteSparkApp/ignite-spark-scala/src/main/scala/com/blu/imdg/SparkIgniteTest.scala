e4b554f7116cc436302c981de1fc6e5dedd3164d
shamim <shamimapache@gmail.com>
2018-09-22 11:52:55 +0300

    Add RDDConsumer

--- chapters/chapter-9/IgniteSparkApp/ignite-spark-scala/src/main/scala/com/blu/imdg/SparkIgniteTest.scala
+++ chapters/chapter-9/IgniteSparkApp/ignite-spark-scala/src/main/scala/com/blu/imdg/SparkIgniteTest.scala
@@ -7,19 +7,23 @@ import org.apache.spark.{SparkConf, SparkContext}
 object RDDProducer extends App {
   val conf = new SparkConf().setAppName("SparkIgniteProducer")
   val sc = new SparkContext(conf)
-  //val ic = new IgniteContext[Int, Int](sc, () => new IgniteConfiguration())
   val ic = new IgniteContext(sc, () => new IgniteConfiguration())
   val sharedRDD: IgniteRDD[Int, Int] = ic.fromCache("IgniteRDD")
   sharedRDD.savePairs(sc.parallelize(1 to 1000, 10).map(i => (i, i)))
+
+  ic.close(true)
+  sc.stop()
 }
 
 object RDDConsumer extends App {
   val conf = new SparkConf().setAppName("SparkIgniteConsume")
   val sc = new SparkContext(conf)
-  //val ic = new IgniteContext[Int, Int](sc, () => new IgniteConfiguration())
   val ic = new IgniteContext(sc, () => new IgniteConfiguration())
-  val sharedRDD = ic.fromCache[String, Int]("IgniteRDD")
+  val sharedRDD = ic.fromCache[Int, Int]("IgniteRDD")
   val lessThanTwenty = sharedRDD.filter(_._2 < 20)
 
   println("The count is:::::::::::: "+lessThanTwenty.count())
+
+  ic.close(true)
+  sc.stop()
 }
