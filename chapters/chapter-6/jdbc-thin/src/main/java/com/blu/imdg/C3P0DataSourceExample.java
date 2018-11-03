44b7dbef93dd2aa500387c627a1b3251ca109b9f
shamim <shamimapache@gmail.com>
2018-11-03 12:46:16 +0300

    Hikari connection pool added

--- chapters/chapter-6/jdbc-thin/src/main/java/com/blu/imdg/C3P0DataSourceExample.java
+++ chapters/chapter-6/jdbc-thin/src/main/java/com/blu/imdg/C3P0DataSourceExample.java
@@ -9,7 +9,24 @@ import java.sql.SQLException;
 
 public class C3P0DataSourceExample {
     private static ComboPooledDataSource cpds;
+
     private static final String JDBC_URL = "jdbc:ignite:thin://127.0.0.1";
+    private static final String SQL_SALARY_MORE_THAN_3000 = "SELECT ename, deptno, job, sal\n" +
+            "FROM EMP\n" +
+            "WHERE (1.25 * sal) > 3000 ;";
+    private static final String SQL_SALARY_SMALLEST="SELECT ename, deptno, sal  \n" +
+            "FROM emp  \n" +
+            "WHERE sal IN  \n" +
+            "( SELECT MIN(sal)  \n" +
+            "FROM emp  \n" +
+            "GROUP BY deptno \n" +
+            ");";
+    private static final String SQL_SALARY_MORE_THAN_AVG="SELECT ename, deptno, sal  \n" +
+            "FROM emp  \n" +
+            "WHERE sal >  \n" +
+            "( SELECT AVG(sal)  \n" +
+            "FROM emp \n" +
+            ");";
 
     static {
         cpds = new ComboPooledDataSource();
@@ -28,22 +45,38 @@ public class C3P0DataSourceExample {
         }
     }
 
-    public static void main(String[] args) throws SQLException{
+    public static void main(String[] args) {
         System.out.println("Data source example!!");
 
         if(cpds!= null){
-            Connection conn = cpds.getConnection();
-            // query some data
-            ResultSet rs = conn.createStatement().executeQuery("SELECT e.ename, e.empno, e.deptno, d.loc FROM emp e, dept d\n" +
-                    "WHERE e.deptno = d.deptno\n" +
-                    "ORDER BY e.ename;");
-            while (rs.next()){
-                String ename = rs.getString(1);
-                System.out.println("Employee name:" + ename);
-            }
 
-            // close connection
-            conn.close();
+            try(Connection conn = cpds.getConnection()){
+                ResultSet rs = conn.createStatement().executeQuery(SQL_SALARY_MORE_THAN_3000);
+
+                System.out.println("A list of the employees whose salary is more than 3000 after giving 25% !!!");
+                while (rs.next()){
+                    System.out.println("Employee name: " + rs.getString(1) + " Department: "+rs.getString(2) + " Job:" + rs.getString(3) + " Salary: " + rs.getString(4));
+                }
+
+                //Execute an query to display the name ( first name and last name ), salary, department id for those employees who earn such amount of salary which is the smallest salary of any of the departments.
+                rs = conn.createStatement().executeQuery(SQL_SALARY_SMALLEST);
+
+                System.out.println("A list of employees who earn such amount of salary which is the smallest salary of any of the departments!!!");
+                while (rs.next()){
+                    System.out.println("Employee name: " + rs.getString(1) + " Department: "+rs.getString(2) + " Salary: " + rs.getString(3));
+                }
+
+                //Execute an query query to display the employee id, employee name (first name and last name ) for all employees who earn more than the average salary.
+                rs = conn.createStatement().executeQuery(SQL_SALARY_MORE_THAN_AVG);
+
+                System.out.println("A list of employees who earn more than the average salary!!!");
+                while (rs.next()){
+                    System.out.println("Employee name: " + rs.getString(1) + " Department: "+rs.getString(2) + " Salary: " + rs.getString(3));
+                }
+
+            } catch(SQLException e){
+                e.printStackTrace();
+            }
         }
     }
 }
--- chapters/chapter-6/jdbc-thin/src/main/java/com/blu/imdg/HikariCPDataSourceExample.java
+++ chapters/chapter-6/jdbc-thin/src/main/java/com/blu/imdg/HikariCPDataSourceExample.java
@@ -8,7 +8,24 @@ import java.sql.SQLException;
 
 public class HikariCPDataSourceExample {
     private static HikariDataSource hds;
+
     private static final String JDBC_URL = "jdbc:ignite:thin://127.0.0.1";
+    private static final String SQL_SALARY_MORE_THAN_3000 = "SELECT ename, deptno, job, sal\n" +
+            "FROM EMP\n" +
+            "WHERE (1.25 * sal) > 3000 ;";
+    private static final String SQL_SALARY_SMALLEST="SELECT ename, deptno, sal  \n" +
+            "FROM emp  \n" +
+            "WHERE sal IN  \n" +
+            "( SELECT MIN(sal)  \n" +
+            "FROM emp  \n" +
+            "GROUP BY deptno \n" +
+            ");";
+    private static final String SQL_SALARY_MORE_THAN_AVG="SELECT ename, deptno, sal  \n" +
+            "FROM emp  \n" +
+            "WHERE sal >  \n" +
+            "( SELECT AVG(sal)  \n" +
+            "FROM emp \n" +
+            ");";
 
     static{
         hds = new HikariDataSource();
@@ -16,22 +33,37 @@ public class HikariCPDataSourceExample {
         hds.setDriverClassName("org.apache.ignite.IgniteJdbcThinDriver");
     }
 
-    public static void main(String[] args) throws SQLException{
+    public static void main(String[] args) {
         System.out.println("HikariDatasource example!");
 
         if(hds != null){
-            Connection conn = hds.getConnection();
-            // query some data
-            ResultSet rs = conn.createStatement().executeQuery("SELECT e.ename, e.empno, e.deptno, d.loc FROM emp e, dept d\n" +
-                    "WHERE e.deptno = d.deptno\n" +
-                    "ORDER BY e.ename;");
-            while (rs.next()){
-                String ename = rs.getString(1);
-                System.out.println("Employee name:" + ename);
-            }
+            try(Connection conn = hds.getConnection()){
+                // Execute an query to list the employees whose salary is more than 3000 after giving 25%
+                ResultSet rs = conn.createStatement().executeQuery(SQL_SALARY_MORE_THAN_3000);
 
-            // close connection
-            conn.close();
+                System.out.println("A list of the employees whose salary is more than 3000 after giving 25% !!!");
+                while (rs.next()){
+                    System.out.println("Employee name: " + rs.getString(1) + " Department: "+rs.getString(2) + " Job:" + rs.getString(3) + " Salary: " + rs.getString(4));
+                }
+
+                //Execute an query to display the name ( first name and last name ), salary, department id for those employees who earn such amount of salary which is the smallest salary of any of the departments.
+                rs = conn.createStatement().executeQuery(SQL_SALARY_SMALLEST);
+
+                System.out.println("A list of employees who earn such amount of salary which is the smallest salary of any of the departments!!!");
+                while (rs.next()){
+                    System.out.println("Employee name: " + rs.getString(1) + " Department: "+rs.getString(2) + " Salary: " + rs.getString(3));
+                }
+
+                //Execute an query query to display the employee id, employee name (first name and last name ) for all employees who earn more than the average salary.
+                rs = conn.createStatement().executeQuery(SQL_SALARY_MORE_THAN_AVG);
+
+                System.out.println("A list of employees who earn more than the average salary!!!");
+                while (rs.next()){
+                    System.out.println("Employee name: " + rs.getString(1) + " Department: "+rs.getString(2) + " Salary: " + rs.getString(3));
+                }
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
         }
     }
 }
