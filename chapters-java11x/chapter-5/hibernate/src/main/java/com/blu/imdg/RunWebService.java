f082a64176ffe846d83b26aa7e55065633a22283
shamim <shamimapache@gmail.com>
2023-02-01 18:14:55 +0300

    Add Java options examples to run on JDK 17

--- chapters-java11x/chapter-5/hibernate/src/main/java/com/blu/imdg/RunWebService.java
+++ chapters-java11x/chapter-5/hibernate/src/main/java/com/blu/imdg/RunWebService.java
@@ -12,6 +12,7 @@ import javax.xml.ws.Endpoint;
 
 /**
  * Created by shamim
+ * java --add-opens=jdk.management/com.sun.management.internal=ALL-UNNAMED --add-opens=java.base/jdk.internal.misc=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.management/com.sun.jmx.mbeanserver=ALL-UNNAMED --add-opens=jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED --add-opens=java.base/sun.reflect.generics.reflectiveObjects=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED -jar ./target/hibernate-1.0-SNAPSHOT-spring-boot.jar
  */
 public class RunWebService {
     public static void main(String[] args) {
